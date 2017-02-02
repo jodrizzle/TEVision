a68f62133d32a5b4f99b6a32477339994d8f9246
Johan Jansen van Vuuren <johanjvv089@gmail.com>
2017-02-02 14:59:13 +0200

    Commit compilable version

--- TEVision/src/Main.hs
+++ TEVision/src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
-import Lib
-import Utils    
+--import Lib
+--import Utils    
 import Control.Monad ( void )
 import qualified OpenCV as CV
 import qualified OpenCV.Internal.Core.Types.Mat as M
@@ -13,21 +13,28 @@ main :: IO ()
 main = do
         args<-getArgs
         let fname= args !! 0
-        imgGS <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("data/"++fname)
-       -- imgG  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/"++fname)
-      --  hmmm  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/"++fname)
-        --putStrLn . show $ :t img
-       -- CV.withWindow "testOriginal" $ \window -> do
-         --       CV.imshow window imgG
-        --        void $ CV.waitKey 100000
-        --CV.withWindow "testGrayscale" $ \window -> do
-        --        CV.imshow window imgGS
-        --        void $ CV.waitKey 100000
-        let cannyImg = CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 imgGS  
+        imgOrig  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/"++fname)
+        imgGS    <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("data/"++fname)
+        --convert imgGS (CV.D) to (CV.S Word8) to ppass to imshow 
+ 
+        CV.withWindow "Original" $ \window -> do
+                CV.imshow window imgOrig
+                void $ CV.waitKey 100000
+        CV.withWindow "Grayscale" $ \window -> do
+                CV.imshow window imgGS
+                void $ CV.waitKey 100000
+        --let cannyImg = CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 imgGS  
         CV.withWindow "testCanny" $ \window -> do
-                CV.imshow window cannyImg
+                --unwrapped <- cannyImg
+                --CV.imshow window $ cannyImg imgGS
                 void $ CV.waitKey 100000     
-               -- imgG <- cvtColor bgr gray circles_1000x625
-               
+           --Expected type: 
+           --M.Mat ('CV.S '['CV.D, 'CV.D]) 'CV.D ('CV.S GHC.Word.Word8)
+           -- Actual type: 
+           --M.Mat ('CV.S '['CV.D, 'CV.D]) 'CV.D 'CV.D    
+--CANNY takes:
+--Mat (S '[h, w]) channels (S Word8)           
+           
+                    
 --cannyImg:: forall shape channels depth (CV.Mat shape channels depth ~ Lambda)=> CV.Mat shape (1) depth
---cannyImg img= CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 img
+cannyImg img= CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 img
