7063ff9a18e3ce8f0fa84863e48de5424e6b5ff3
Johan Jansen van Vuuren <johanjvv089@gmail.com>
2017-02-02 11:39:33 +0200

    Add scientific package to build-depends in .cabal AND added extra-deps in stack.yaml using ghc resolver.  Switched back to LTS-7.18 resolver.

--- TEVision/src/Main.hs
+++ TEVision/src/Main.hs
@@ -1,20 +1,33 @@
 module Main where
+
+import Lib
+import Utils    
 import Control.Monad ( void )
 import qualified OpenCV as CV
+import qualified OpenCV.Internal.Core.Types.Mat as M
 import qualified Data.ByteString as B 
-import System.Environment            
+import System.Environment    
+
+--myImg = CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/box.png")
 main :: IO ()
 main = do
         args<-getArgs
         let fname= args !! 0
         imgGS <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("data/"++fname)
-        imgG <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/"++fname)
+       -- imgG  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/"++fname)
+      --  hmmm  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/"++fname)
         --putStrLn . show $ :t img
-        CV.withWindow "testOriginal" $ \window -> do
-                CV.imshow window imgG
-                void $ CV.waitKey 100000
-        CV.withWindow "testGrayscale" $ \window -> do
-                CV.imshow window imgGS
-                void $ CV.waitKey 100000
-                
+       -- CV.withWindow "testOriginal" $ \window -> do
+         --       CV.imshow window imgG
+        --        void $ CV.waitKey 100000
+        --CV.withWindow "testGrayscale" $ \window -> do
+        --        CV.imshow window imgGS
+        --        void $ CV.waitKey 100000
+        let cannyImg = CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 imgGS  
+        CV.withWindow "testCanny" $ \window -> do
+                CV.imshow window cannyImg
+                void $ CV.waitKey 100000     
                -- imgG <- cvtColor bgr gray circles_1000x625
+               
+--cannyImg:: forall shape channels depth (CV.Mat shape channels depth ~ Lambda)=> CV.Mat shape (1) depth
+--cannyImg img= CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 img
