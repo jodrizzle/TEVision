c7382b7e5b55e4b6921019d152f733173961faf6
Johan Jansen van Vuuren <johanjvv089@gmail.com>
2017-02-06 15:50:31 +0200

    Obtain vector of contour points

--- TEVision/README.md
+++ TEVision/README.md
@@ -2,16 +2,23 @@ Author:Johan Jansen van Vuuren
 Version:0.1
 Date:07/02/2016
 
-Image source files should be in the "data/" directory.
-
 Usage:
  - stack exec TEVision <filename.extension> <blur_region_size>
 
 Features:
  - Load images (grayscale,color)
  - Canny edge detection
- - Blurring prior to canny edgeing
+ - Blurring prior to canny edge detection
+ - Find contours
  
 Notes:
+ - image source files should be in the "data/" directory.
  - edge detection works slightly better if a grayscale image is used.
  - blur region size can be made very big for good quality photos.  Has to be odd number.
+
+TODO:
+ - Draw contours
+ - Threshold
+ - Crop 
+ - 4 point transform
+ 
--- TEVision/src/Main.hs
+++ TEVision/src/Main.hs
@@ -1,13 +1,21 @@
 module Main where
- 
-import Control.Monad ( void )
+
+import qualified Data.ByteString as B 
 import qualified OpenCV as CV
 import qualified OpenCV.Internal.Core.Types.Mat as M
-import qualified Data.ByteString as B 
-import System.Environment 
-import GHC.Word  
+import qualified OpenCV.ImgProc.StructuralAnalysis as SA 
+import qualified Data.Vector as V
+--import ApproxPolyDP
+import Control.Monad ( void )
+import Control.Monad.Primitive
+--import ConvexHull
 import GHC.Int 
+import GHC.Word  
 import Linear.V2
+--import RamerDouglasPeuckerParts
+--import RightReceipt 
+import System.Environment 
+--  import Utils
 
 main :: IO ()
 main = do
@@ -15,19 +23,45 @@ main = do
         args<-getArgs
         let fname = args !! 0 --filename is first argument
         let blurRegion = args !! 1 --blur kernel size is second argument
-        
         --Read image
         imgOrig  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("../data/"++fname)      
         imgGS    <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("../data/"++fname)
         --tighten matrix type constraints to work with canny and blur
         let formImg = M.unsafeCoerceMat imgGS :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8)
         let kernel = getKernel blurRegion
+        let canniedImg = cannyImg (gaussianBlurImg formImg kernel)
+        contours <- (findingContours canniedImg)
+        putStrLn (show contours)
+        imgMut <-  CV.thaw imgOrig
+        --contourImg <- CV.drawContours (V.map SA.contourPoints contours) (CV.toScalar (0::Double,255::Double,0::Double)) (CV.OutlineContour CV.LineType_AA 1) imgMut
+        
+      --  let listOfPoints   = getListOfPoints (V.toList contours)
+      --  let listOfAreas    = dictAreaPoints listOfPoints
+      --  let screenContours = findReceipt listOfAreas
+
+      --  putStrLn (show screenContours)
+        
+        --display results
+        --showImage "Contours" contourImg
         showImage "Original" imgOrig
         showImage "Grayscale" imgGS
         showImage "Edges" $ cannyImg formImg
         showImage "Edges (median blur)" $ cannyImg (medianBlurImg formImg kernel)
-        showImage "Edges (Gaussian blur)" $  cannyImg (gaussianBlurImg formImg kernel)
-                                  
+        showImage "Edges (Gaussian blur)" canniedImg
+      
+
+{-
+drawContours:: (ToScalar color, PrimMonad m)=> Vector (Vector Point2i)-> color-> ContourDrawMode-> Mut (Mat (S '[h, w]) channels depth) (PrimState m) -> m ()    
+drawContours (V.map contourPoints contours)
+                        ())
+                        (OutlineContour LineType_AA 1)
+                        imgM
+   
+   -}      
+
+      
+      
+      
 getKernel::String->Int32
 getKernel reg = read reg
 
@@ -37,7 +71,7 @@ showImage title img = CV.withWindow title $ \window -> do  --display image
                         void $ CV.waitKey 100000
                         
 cannyImg::M.Mat (CV.S '[h, w]) channels (CV.S Word8)->(M.Mat (CV.S '[h, w]) (CV.S 1) (CV.S Word8))  
-cannyImg img = CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 img
+cannyImg img = CV.exceptError $ CV.canny 75 200 Nothing CV.CannyNormL2 img
 
 medianBlurImg:: (depth `CV.In` '[Word8, Word16, Float], channels `CV.In` '[1, 3, 4]) => (M.Mat shape ('CV.S channels) ('CV.S depth))->Int32-> M.Mat shape ('CV.S channels) ('CV.S depth)
 medianBlurImg imgO regionSize = CV.exceptError $ CV.medianBlur imgO regionSize
@@ -45,3 +79,8 @@ medianBlurImg imgO regionSize = CV.exceptError $ CV.medianBlur imgO regionSize
 gaussianBlurImg:: (depth `CV.In` '[Word8, Word16, Float, Double], channels `CV.In` '[1, 3, 4])  => (M.Mat shape ('CV.S channels) ('CV.S depth))->Int32-> M.Mat shape ('CV.S channels) ('CV.S depth)
 gaussianBlurImg imgO size = CV.exceptError $ CV.gaussianBlur (V2 size size ::V2 Int32) 0 0 imgO 
             
+findingContours :: PrimMonad m => M.Mat ('CV.S '[h0, w0]) ('CV.S 1) ('CV.S Word8) -> m (V.Vector SA.Contour)
+findingContours image = do
+                    imageM <- CV.thaw image
+                    contours_vector <- CV.findContours SA.ContourRetrievalList SA.ContourApproximationSimple imageM
+                    pure contours_vector
