9d15cd5dbebcfe49790e60adfbc31d49ac5e86af
Johan Jansen van Vuuren <johanjvv089@gmail.com>
2017-02-06 10:28:44 +0200

    Add median blurring prior to edge detection.  Add command line argument for blur region size.

--- TEVision/README.md
+++ TEVision/README.md
@@ -1,6 +1,15 @@
-
+Author:Johan Jansen van Vuuren
+Version:0.1
+Date:07/02/2016
 
 Image source files should be in the "data/" directory.
 
 Usage:
 stack exec TEVision <filename.extension>
+
+Features:
+ - Load images (grayscale,color)
+ - Canny edge detection
+ 
+Notes:
+edge detection works slightly better if a grayscale image is used.
--- TEVision/src/Main.hs
+++ TEVision/src/Main.hs
@@ -11,8 +11,9 @@ main :: IO ()
 main = do
         args<-getArgs
         let fname = args !! 0
-        imgOrig  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("data/"++fname)      
-        imgGS    <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("data/"++fname)
+        let blurRegion = args !! 1
+        imgOrig  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("../data/"++fname)      
+        imgGS    <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("../data/"++fname)
         
        -- blurred <- medianBlurImg (M.unsafeCoerceMat imgOrig :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8))
         CV.withWindow "Original" $ \window -> do  --display original image
@@ -21,13 +22,11 @@ main = do
         CV.withWindow "Grayscale" $ \window -> do --display image in grayscale
                 CV.imshow window imgGS  
                 void $ CV.waitKey 100000
-         
-         
-  
-        --let changedMat = CV.exceptError $ M.coerceMat imgGS  --returns -> CvExcept (Mat shapeOut channelsOut depthOut)
-        CV.withWindow "testCanny" $ \window -> do --display canny edge detected image                
-         --       CV.imshow window blurred --cannyImg imgFormatted  --cannyImg requires Mat (S '[h, w]) channels (S Word8)
-                CV.imshow window $ cannyImg (M.unsafeCoerceMat imgOrig :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8))
+        CV.withWindow "Canny edges" $ \window -> do --display canny edge detected image      
+                CV.imshow window $ cannyImg (M.unsafeCoerceMat imgGS :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8))
+                void $ CV.waitKey 100000
+        CV.withWindow "Median blurring then canny edges" $ \window -> do --display canny edge detected image      
+                CV.imshow window $ cannyImg (medianBlurImg (M.unsafeCoerceMat imgGS :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8)) (read blurRegion))
                 void $ CV.waitKey 100000     
                  {-imshow:: Window	 
                            -> Mat (S '[height, width]) channels depth	 
@@ -37,10 +36,10 @@ main = do
 --      cannyImg:: forall shape channels depth. (M.Mat shape channels depth ~ Img)=> M.Mat shape ('CV.S 1) depth
 cannyImg img = CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 img
 {-
-medianBlurImg:: (depth `CV.In` '[Word8, Word16, Float], channels `CV.In` '[1, 3, 4]) => (M.Mat shape ('CV.S channels) ('CV.S depth))-> CV.CvExcept(M.Mat shape ('CV.S channels) ('CV.S depth)) 
-medianBlurImg imgO = CV.medianBlur imgO 13
+medianBlurImg:: (depth `CV.In` '[Word8, Word16, Float], channels `CV.In` '[1, 3, 4]) => (M.Mat shape ('CV.S channels) ('CV.S depth))-> CV.CvExcept(M.Mat shape ('CV.S channels) ('CV.S depth))-} 
+medianBlurImg imgO regionSize = CV.exceptError $ CV.medianBlur imgO regionSize
  
--}
+
 --let     imgFormatted::(Double dstDepth0)=> CV.CvExcept (M.Mat a b dstDepth0)
         --        imgFormatted = formatShow imgOrig
         --let imgForm = CV.coerceMat imgOrig
@@ -54,8 +53,4 @@ medianBlurImg imgO = CV.medianBlur imgO 13
                                 -> Mat shape channels srcDepth	 
                                 -> CvExcept (Mat shape channels dstDepth)	 
                                 Converts an array to another data type with optional scaling-}
-           --Expected type: 
-           --M.Mat ('CV.S '['CV.D, 'CV.D]) 'CV.D ('CV.S GHC.Word.Word8)
-           --Actual type: 
-           --M.Mat ('CV.S '['CV.D, 'CV.D]) 'CV.D 'CV.D         
---formatShow::{-(Fractional destDepth)=>-}(M.Mat shape0 channels0 depth0)->(CV.CvExcept (M.Mat shape channels depth))      
+            
