6f1cc951910d7bb887e13ca9d95263703f100b43
Johan Jansen van Vuuren <johanjvv089@gmail.com>
2017-02-06 13:12:50 +0200

    Add Gaussian blur and modular window show function "showImage"

--- TEVision/README.md
+++ TEVision/README.md
@@ -5,11 +5,13 @@ Date:07/02/2016
 Image source files should be in the "data/" directory.
 
 Usage:
-stack exec TEVision <filename.extension>
+ - stack exec TEVision <filename.extension> <blur_region_size>
 
 Features:
  - Load images (grayscale,color)
  - Canny edge detection
+ - Blurring prior to canny edgeing
  
 Notes:
-edge detection works slightly better if a grayscale image is used.
+ - edge detection works slightly better if a grayscale image is used.
+ - blur region size can be made very big for good quality photos.  Has to be odd number.
--- TEVision/src/Main.hs
+++ TEVision/src/Main.hs
@@ -5,52 +5,42 @@ import qualified OpenCV as CV
 import qualified OpenCV.Internal.Core.Types.Mat as M
 import qualified Data.ByteString as B 
 import System.Environment 
-import GHC.Word   
+import GHC.Word  
+import GHC.Int 
+import Linear.V2
 
 main :: IO ()
 main = do
+        --Parse arguments
         args<-getArgs
-        let fname = args !! 0
-        let blurRegion = args !! 1
+        let fname = args !! 0 --filename is first argument
+        let blurRegion = args !! 1 --blur kernel size is second argument
+        
+        --Read image
         imgOrig  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("../data/"++fname)      
         imgGS    <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("../data/"++fname)
-        
-       -- blurred <- medianBlurImg (M.unsafeCoerceMat imgOrig :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8))
-        CV.withWindow "Original" $ \window -> do  --display original image
-                CV.imshow window imgOrig
-                void $ CV.waitKey 100000
-        CV.withWindow "Grayscale" $ \window -> do --display image in grayscale
-                CV.imshow window imgGS  
-                void $ CV.waitKey 100000
-        CV.withWindow "Canny edges" $ \window -> do --display canny edge detected image      
-                CV.imshow window $ cannyImg (M.unsafeCoerceMat imgGS :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8))
-                void $ CV.waitKey 100000
-        CV.withWindow "Median blurring then canny edges" $ \window -> do --display canny edge detected image      
-                CV.imshow window $ cannyImg (medianBlurImg (M.unsafeCoerceMat imgGS :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8)) (read blurRegion))
-                void $ CV.waitKey 100000     
-                 {-imshow:: Window	 
-                           -> Mat (S '[height, width]) channels depth	 
-                           -> IO ()	 
-                 -}
+        --tighten matrix type constraints to work with canny and blur
+        let formImg = M.unsafeCoerceMat imgGS :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 3) ('CV.S Word8)
+        let kernel = getKernel blurRegion
+        showImage "Original" imgOrig
+        showImage "Grayscale" imgGS
+        showImage "Edges" $ cannyImg formImg
+        showImage "Edges (median blur)" $ cannyImg (medianBlurImg formImg kernel)
+        showImage "Edges (Gaussian blur)" $  cannyImg (gaussianBlurImg formImg kernel)
                                   
---      cannyImg:: forall shape channels depth. (M.Mat shape channels depth ~ Img)=> M.Mat shape ('CV.S 1) depth
-cannyImg img = CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 img
-{-
-medianBlurImg:: (depth `CV.In` '[Word8, Word16, Float], channels `CV.In` '[1, 3, 4]) => (M.Mat shape ('CV.S channels) ('CV.S depth))-> CV.CvExcept(M.Mat shape ('CV.S channels) ('CV.S depth))-} 
-medianBlurImg imgO regionSize = CV.exceptError $ CV.medianBlur imgO regionSize
- 
+getKernel::String->Int32
+getKernel reg = read reg
 
---let     imgFormatted::(Double dstDepth0)=> CV.CvExcept (M.Mat a b dstDepth0)
-        --        imgFormatted = formatShow imgOrig
-        --let imgForm = CV.coerceMat imgOrig
-        --convert imgGS (CV.D) to (CV.S Word8) to pass to imshow 
---formatShow img = CV.matConvertTo Nothing Nothing img  
- --toDepthDS :: a -> DS Depth
-                 {-
-                 matConvertTo:: ToDepthDS (Proxy dstDepth)	 
-                                => Maybe Double	        Optional scale factor.
-                                -> Maybe Double	        Optional delta added to the scaled values.
-                                -> Mat shape channels srcDepth	 
-                                -> CvExcept (Mat shape channels dstDepth)	 
-                                Converts an array to another data type with optional scaling-}
+showImage title img = CV.withWindow title $ \window -> do  --display image
+                        CV.imshow window img
+                        void $ CV.waitKey 100000                                  
+--showImage title = CV.withWindow title $ \window -> do  --display empty window
+  --                      void $ CV.waitKey 100000                                 
+cannyImg img = CV.exceptError $ CV.canny 30 200 Nothing CV.CannyNormL1 img
+
+medianBlurImg:: (depth `CV.In` '[Word8, Word16, Float], channels `CV.In` '[1, 3, 4]) => (M.Mat shape ('CV.S channels) ('CV.S depth))->Int32-> M.Mat shape ('CV.S channels) ('CV.S depth)
+medianBlurImg imgO regionSize = CV.exceptError $ CV.medianBlur imgO regionSize
+
+gaussianBlurImg:: (depth `CV.In` '[Word8, Word16, Float, Double], channels `CV.In` '[1, 3, 4])  => (M.Mat shape ('CV.S channels) ('CV.S depth))->Int32-> M.Mat shape ('CV.S channels) ('CV.S depth)
+gaussianBlurImg imgO size = CV.exceptError $ CV.gaussianBlur (V2 size size ::V2 Int32) 0 0 imgO 
             
