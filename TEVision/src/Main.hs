260a9637b65e687c413fb9b572777e6c295dedcf
Johan Jansen van Vuuren <johanjvv089@gmail.com>
2017-02-13 15:12:07 +0200

    Add crop method with hardcoded region.

--- TEVision/src/Main.hs
+++ TEVision/src/Main.hs
@@ -8,50 +8,56 @@ import qualified OpenCV.ImgProc.StructuralAnalysis as SA
 import qualified Data.Vector as V
 import Control.Monad (void)
 import Control.Monad.Primitive
+import Data.Int
 import Data.List
 import Data.Map
 import Data.Maybe    
+import Data.Proxy
 import Filters
 import GHC.Int (Int32)
 import GHC.Word  
+import Graphics.UI.GLFW
 import Linear
 import RightReceipt_CA (findCoords,getListOfPoints)
 import System.Environment
 import Utilities
 import Utils_CA (dictAreaPoints)
+import Foreign.C.Types
+import OpenCV.Internal.C.Types
 
 main :: IO ()
 main = do
-      --Parse arguments
+      --Parse arguments-------------------------------------------------------------------------------
         args<-getArgs
         let fname = args !! 0 --filename is first argument
         let blurRegion = args !! 1 --blur kernel size is second argument
         
-      --Read image
+      --Read image------------------------------------------------------------------------------------
         imgOrig  <- CV.imdecode CV.ImreadUnchanged <$> B.readFile ("../data/"++fname)      
         imgGS    <- CV.imdecode CV.ImreadGrayscale <$> B.readFile ("../data/"++fname)
         
-      --tighten matrix type constraints to work with canny and blur
+      --tighten matrix type constraints to work with canny and blur-----------------------------------
         let formImg = CV.exceptError $ M.coerceMat imgGS :: M.Mat ('CV.S '[ 'CV.D, 'CV.D]) ('CV.S 1) ('CV.S Word8)
         let kernel = getKernel blurRegion
         let canniedImg = cannyImg (gaussianBlurImg formImg kernel)
       
-      --detect and draw contours
+      --detect and draw contours----------------------------------------------------------------------
         contours <- (getContours canniedImg)
         imgMut <- CV.thaw imgOrig--make mutable matrix  
         --let red = CV.toScalar (V4   0   0 255 255 :: V4 Double)--color for drawContours
         CV.drawContours (V.map SA.contourPoints contours) red (CV.OutlineContour CV.LineType_AA 5) imgMut --action to mutate imgMut
         contoured_img <- CV.freeze imgMut--make matrix immutable again
       --putStrLn $ show $ V.length $ SA.contourPoints $ contours V.! 0 --first contour in contours (Vector of contours;each contour is a Vector of point2i)
-      --putStrLn $ show $ V.length contours --print length of vector of contours "how many contours detected?".  One contour consists of many points
+        putStrLn $ "Number of outlines detected: " ++ (show $ V.length contours) --print length of vector of contours "how many contours detected?".  One contour consists of many points
       --putStrLn $ show $ SA.contourPoints $ contours V.! 0
-      
+        
       --let listOfPoints   = (V.toList contours)
       --putStrLn $ show $ listOfPoints
       --let listOfAreas    = dictAreaPoints listOfPoints
       --let screenContours = findCoords listOfAreas
       --putStrLn $ show rotationMatrix
-      --display results
+      --putStrLn $ show $ inContour (SA.contourPoints $ contours V.! 0) $ P.toPoint (V2 0.0 0.0)        --Use matConvertTo to change depth
+      --display results-------------------------------------------------------------------------------
         showImage "Original" $ imgOrig
       --showImage "Warped" $ warpAffineImg imgOrig --transform
       --showImage "Warped" $ warpAffineInvImg $ warpAffineImg imgOrig --apply inverse transform on transformed image
@@ -61,3 +67,29 @@ main = do
       --showImage "Edges (median blur)" $ cannyImg (medianBlurImg formImg kernel)
         showImage "Edges (Gaussian blur)" canniedImg
         showImage "Contours" contoured_img
+
+        showImage "Cropped" $ cropImg imgOrig
+        
+
+-- matFromFuncImg = CV.exceptError $ CV.matFromFunc 
+--       (Proxy :: Proxy [size, size])
+--       (Proxy :: Proxy 4)
+--       (Proxy :: Proxy Word8)
+--       example
+--   where
+--     example [y, x] 0 = 255 - normDist (V2 x y ^-^ bluePt )
+--     example [y, x] 1 = 255 - normDist (V2 x y ^-^ greenPt)
+--     example [y, x] 2 = 255 - normDist (V2 x y ^-^ redPt  )
+--     example [y, x] 3 =       normDist (V2 x y ^-^ alphaPt)
+--     example _pos _channel = error "impossible"
+-- 
+-- normDist :: V2 Int -> Word8
+-- normDist v = floor $ min 255 $ 255 * Linear.norm (fromIntegral <$> v) / s'
+-- 
+-- bluePt  = V2 0 0
+-- greenPt = V2 s s
+-- redPt   = V2 s 0
+-- alphaPt = V2 0 s
+-- 
+-- s = fromInteger $ natVal (size) :: Int
+-- s' = fromIntegral s :: Double
--- TEVision/src/Utilities.hs
+++ TEVision/src/Utilities.hs
@@ -1,7 +1,8 @@
-module Utilities (
-    getContours
+module Utilities (  
+    cropImg
+   ,getContours
+   ,inContour 
    ,laplacianFilter 
-   --,remapImage
    ,rotationMatrix
    ,showImage
    ,warpAffineImg
@@ -16,16 +17,21 @@ module Utilities (
 
 import Control.Monad (void)
 import Control.Monad.Primitive
+import Data.Proxy
 import GHC.Int (Int32)
 import GHC.Word 
+import Filters
 import Linear
 import System.Environment 
 import qualified OpenCV as CV
+import Foreign.C.Types 
+import qualified OpenCV.Core.Types.Point as P
+import OpenCV.Internal.C.Types
 import qualified OpenCV.Internal.Core.Types.Mat as M
 import qualified OpenCV.ImgProc.StructuralAnalysis as SA 
 import qualified Data.Vector as V
 
-transparent, white, black, blue, green, red :: CV.Scalar
+transparent, white, black, blue, green, red   :: CV.Scalar
 transparent = CV.toScalar (V4 255 255 255   0 :: V4 Double)
 white       = CV.toScalar (V4 255 255 255 255 :: V4 Double)
 black       = CV.toScalar (V4   0   0   0 255 :: V4 Double)
@@ -33,12 +39,22 @@ blue        = CV.toScalar (V4 255   0   0 255 :: V4 Double)
 green       = CV.toScalar (V4   0 255   0 255 :: V4 Double)
 red         = CV.toScalar (V4   0   0 255 255 :: V4 Double)
 
+subRect= CV.toRect $ CV.HRect (V2 50 131) (V2 190 60)
+      
+cropImg::M.Mat (CV.S '[height, width]) channels depth -> (M.Mat (CV.S '[CV.D, CV.D]) channels depth)
+cropImg img =  CV.exceptError $ CV.matSubRect img subRect        
+
 getContours :: PrimMonad m => M.Mat ('CV.S '[h0, w0]) ('CV.S 1) ('CV.S Word8) -> m (V.Vector SA.Contour)
 getContours image = do
                     imageM <- CV.thaw image
                     contours_vector <- CV.findContours SA.ContourRetrievalExternal SA.ContourApproximationTC89KCOS imageM
                     pure contours_vector
-
+                    
+inContour:: (CV.IsPoint2 contourPoint2 CFloat, CV.IsPoint2 testPoint2 CFloat)=> V.Vector (contourPoint2 CFloat)->testPoint2 CFloat -> Bool
+inContour cont pt
+    | (CV.exceptError $ SA.pointPolygonTest cont pt False) >=0 = True
+    | otherwise = False
+    
 --has to be grayscale input                    
 laplacianFilter::M.Mat shape ('CV.S 1) ('CV.S Word8)-> M.Mat shape ('CV.S 1) ('CV.S Word8)
 laplacianFilter img = CV.exceptError $ CV.laplacian Nothing Nothing Nothing Nothing img
@@ -52,6 +68,7 @@ rotationMatrix = CV.getRotationMatrix2D (V2 256 170 ) 30 0.6
 showImage::String-> M.Mat (CV.S '[height, width]) channels depth-> IO ()
 showImage title img = CV.withWindow title $ \window -> do  --display image
                         CV.imshow window img
+                        CV.resizeWindow window 500 500
                         void $ CV.waitKey 100000
 
 warpAffineImg :: M.Mat (CV.S '[height, width]) channels depth->(M.Mat (CV.S '[height, width]) channels depth)
@@ -59,3 +76,21 @@ warpAffineImg img = CV.exceptError $ CV.warpAffine img rotationMatrix CV.InterAr
 
 warpAffineInvImg ::  M.Mat (CV.S '[height, width]) channels depth->(M.Mat (CV.S '[height, width]) channels depth)
 warpAffineInvImg img= CV.exceptError $ CV.warpAffine img rotationMatrix CV.InterCubic True False (CV.BorderConstant black)
+
+
+
+
+
+
+
+-- maskFunction::[Int] -> Int -> M.StaticDepthT depth
+-- maskFunction [y, x] 0
+--     | (inContour $ P.toPoint (V2 x y :: V2 CFloat) CFloat) == True = [y, x]
+--     | otherwise = [0, 0]
+-- maskFunction [y, x] 1  
+--     | (inContour $ P.toPoint (V2 x y :: V2 CFloat) CFloat) == True = [y, x]
+--     | otherwise = [0, 0]
+-- maskFunction _pos _channel =  error "impossible"
+-- 
+-- remapImg:: V.Vector SA.Contour -> M.Mat ('CV.S ['CV.S height, 'CV.S width]) ('CV.S channels) ('CV.S depth) -> M.Mat ('CV.S ['CV.S height, 'CV.S width]) ('CV.S channels) ('CV.S depth)
+-- remapImg contours img = CV.exceptError $ CV.remap img (CV.matFromFunc (Proxy :: Proxy [height, width]) (Proxy :: Proxy 2) (Proxy :: Proxy Float) maskFunction) CV.InterLinear (CV.BorderConstant black)
